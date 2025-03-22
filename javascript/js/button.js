e478e510bf39bf9b0b144f04f66696f51d02f49b
Bai Kimhouy <ctin871@gmail.com>
2025-03-22 19:19:22 +0700

    Add files via upload

---
+++ javascript/js/button.js
@@ -0,0 +1,5 @@
+const title = document.querySelector("h1");
+const buttton = document.querySelector("button");
+
+let Array = (1,2,3);
+console.log("Array");
---
+++ javascript/js/javascrip.js
@@ -0,0 +1,95 @@
+// //va,let
+// // var name = "Houy";
+// // console.log("name :" , name);
+
+// // let StudentID = "A01";
+// // console.log("ID :",StudentID);
+// //forward 
+// // function add (a , b){
+// //     var c = a + b;
+// //     if(c > 1){
+// //         var d = c + 1;
+// //     } else{
+// //         console.log(" c : ", c);
+// //     }
+// // }
+// // add (2 , 0);
+// // body = document.getElementsByName("body");
+// // console.log(body);
+// // body.innerText = "hello";
+// //object.assign for assign color to object jrern 
+// // const title = document.querySelector("dis");
+// // // title.innerText = "Tourn Vuthy";
+// // // title.innerHTML = "<i> Vuthy </i>"
+// // title.style.backgroundColor = "pink";
+// //for each for loop element del yg ot dg pi loop
+// //for loop is for loop del yg dg jom nun
+// //array and object are not primitive type 
+// //we can assign value to string in 3 different way : "", '',
+// // Assignment operator
+
+// // let a = 3;
+// // let b = 4;
+
+// // console.log("a: ", a);
+// // b **= a; // b= b^a;
+// // console.log("b: ", b);
+// // b /= a; // b= b/a;
+// // console.log("b: ", b);
+// // b += a; // b= b+a;
+// // console.log("b: ", b);
+// // b -= a; // b= b-a;
+// // console.log("b: ", b);
+// // b *= a; // b= b*a;
+// // console.log("b: ", b);
+// // b %= a; // b = b % a
+// // console.log("b: ", b);
+
+// //comparision operator
+// // let a = 1;
+// // let b = "1";
+// // console.log(a === b);
+// //logical operator
+// //>= <=
+// // String operator
+// // ternary operator
+// // let score = 50;
+// // if(score < 50) {
+// //     console.log("Fail");
+// // }else{
+// //     console.log("pass");}
+
+// // //condition  ? true : false
+// // let result = score < 50 ? console.log("fail") : console.log("pass");
+// // Nullish coalescing use to assign default value to NULL or UNDEFINE variable
+// let a;
+
+// a ??= 0;
+
+//  || operator use to assign default value to variable that contain falsy value;
+
+// let a = "" || "Empty string";
+
+// console.log("a:  ", a);
+//array
+// let array1 = new Array();
+// console.log('array1 ', array1);
+// let array2 = [1, 2 , "a" , "b" ];
+// array2[0] = 9;//chang value array
+// console.log('array2', array2[0]);
+// let array = [1,2,3,4];
+// array.unshift(5);
+// array.push(5);
+// array.pop(5);
+// array[1] *= 2;
+// console.log("array" , array);
+// let array2 = [];
+// array2 =array.splice();
+// console.log("array2", array2);
+// const myHeading = document.querySelector("h1");
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+  alert("Yay, I love chocolate ice cream!");
+} else {
+  alert("Awwww, but chocolate is my favorite…");
+}
